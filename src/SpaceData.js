fd824e0cf19cf9705072168e0ba1b770499d5030
GageaMiruna <gageamiruna@gmail.com>
2019-05-23 18:58:00 +0300

    SpaceProject

---
+++ src/SpaceData.js
@@ -0,0 +1,32 @@
+import { LitElement, html, css } from 'lit-element';
+
+class SpaceData extends LitElement {
+  static get styles() {
+    return css`
+    //hoste e un fel de this, host e elementul custom definit de noi
+      :host {
+        background: lightblue;
+        display: block;
+        padding: 1rem;
+      }
+    `;
+  }
+
+  static get properties() {
+    return {
+      lat: { type: Number },
+      lon: { type: Number },
+      place: { type: String },
+    };
+  }
+
+  render() {
+    return html`
+      <div>Lat: <output>${this.lat}</output></div>
+      <div>Lon: <output>${this.lon}</output></div>
+      <div>Place: <span>${this.place}</span></div>
+    `;
+  }
+}
+
+window.customElements.define('space-data', SpaceData);
